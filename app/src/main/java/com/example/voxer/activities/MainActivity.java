8500242cb061ee35f4a61731b597858fcaa25d64
GravenHawkX <aaron.augustine@xaviers.edu.in>
2023-04-04 18:54:37 +0530

    New background and Layout Added bg image file

--- app/src/main/java/com/example/voxer/activities/MainActivity.java
+++ app/src/main/java/com/example/voxer/activities/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.voxer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.example.voxer.R;
+import com.example.voxer.databinding.ActivityMainBinding;
+import com.example.voxer.utilities.Constants;
+import com.example.voxer.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager((getApplicationContext()));
+        loadUserDetails();
+    }
+
+    private void loadUserDetails(){
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
     }
 }
