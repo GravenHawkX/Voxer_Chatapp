8e5e2f207514979a703b136034a37ba04d32fb6b
daniel <daniel.pereira@xaviers.edu.in>
2023-04-05 18:59:51 +0530

    Adding test cases

--- app/src/androidTest/java/com/example/voxer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/voxer/ExampleInstrumentedTest.java
@@ -1,14 +1,25 @@
 package com.example.voxer;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.doubleClick;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import com.example.voxer.activities.SignInActivity;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +28,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<SignInActivity> activityScenarioRule = new ActivityScenarioRule<>(SignInActivity.class);
+
+
     @Test
-    public void useAppContext() {
+    public void successfulLogin() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.voxer", appContext.getPackageName());
+        onView(withId(R.id.InputEmail)).perform(typeText("daniel.pereira@xaviers.edu.in"));
+        onView(withId(R.id.InputPassword)).perform(typeText("dan123"));
+        pressBack();
+        onView(withId(R.id.buttonSignIn)).perform(doubleClick());
     }
 }
